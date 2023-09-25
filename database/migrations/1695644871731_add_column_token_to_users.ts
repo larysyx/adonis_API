178542af452778570904edcf3d00e33dd2c504b6
larysyx <lauragabriela.lemos3@gmail.com>
2023-09-25 10:38:57 -0300

    25.09

--- database/migrations/1695644871731_add_column_token_to_users.ts
+++ database/migrations/1695644871731_add_column_token_to_users.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'token'
+  protected tableName = 'users'
 
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
