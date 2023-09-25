7a091d5e018e924b60810b43a8590908c89dd20a
larysyx <lauragabriela.lemos3@gmail.com>
2023-09-25 10:11:07 -0300

    25.09

--- database/migrations/1695644871731_add_column_token_to_users.ts
+++ database/migrations/1695644871731_add_column_token_to_users.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('users')
+      table.string('token')
     })
   }
 
