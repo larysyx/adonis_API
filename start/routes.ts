a75ac5136f51bf4c5ab51e9daa63a85178290d5c
larysyx <lauragabriela.lemos3@gmail.com>
2023-10-30 10:01:04 -0300

    30.10

--- start/routes.ts
+++ start/routes.ts
@@ -7,5 +7,5 @@ Route.get('/', async () => {
 Route.post("/register", "AuthController.register")
 Route.post("/login", "AuthController.login")
 Route.group(() => {
-  Route.put("/user", "UserController.update")
+  Route.put("/user", "UsersController.update")
 }).middleware('auth')
