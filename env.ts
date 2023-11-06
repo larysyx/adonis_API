136e6e4995bde85fb59d69909842ace91ad9b688
larysyx <lauragabriela.lemos3@gmail.com>
2023-11-06 09:36:18 -0300

    06.11

--- env.ts
+++ env.ts
@@ -19,6 +19,9 @@ export default Env.rules({
 	PORT: Env.schema.number(),
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
+  	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+	QUEUE_REDIS_HOST: Env.schema.string({ format: 'host' }),
+	QUEUE_REDIS_PORT: Env.schema.number(),
+	QUEUE_REDIS_PASSWORD: Env.schema.string.optional(),
 })
